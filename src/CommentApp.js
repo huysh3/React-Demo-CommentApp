3c1898f8018efdc5029fd01d717ccad042658a6b
胡钰盛 <wilson.hu@shendun.info>
2017-04-25 12:22:03 +0800

    dev

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import CommentInput from './CommentInput'
-import CommentList from './CommentList'
+import React, {Component} from 'react';
+import CommentInput from './CommentInput';
+import CommentList from './CommentList';
 
 class CommentApp extends Component {
-    render() {
-        return (
-            <div className="commentAppContainer">
-                <CommentInput />
-                <CommentList />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <CommentInput />
+        <CommentList />
+      </div>
+    );
+  }
 }
 
-export default CommentApp
+export default CommentApp;
